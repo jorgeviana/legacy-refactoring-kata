ff23f4eed4f1a485f88e4716c2df101dae52be67
Jorge Viana <secoviana@gmail.com>
2018-10-25 16:44:03 +0200

    TripService - should throw an exception when a not logged in user tries to view the trips of any other user

--- complete/src/main/java/refactoring/kata/trip/TripService.java
+++ complete/src/main/java/refactoring/kata/trip/TripService.java
@@ -11,7 +11,7 @@ public class TripService {
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
 		List<Trip> tripList = new ArrayList<>();
-		User loggedUser = UserSession.getInstance().getLoggedUser();
+		User loggedUser = getLoggedInUser();
 		boolean isFriend = false;
 		if (loggedUser != null) {
 			for (User friend : user.getFriends()) {
@@ -27,5 +27,9 @@ public class TripService {
 		} else {
 			throw new UserNotLoggedInException();
 		}
+	}
+
+	protected User getLoggedInUser() {
+		return UserSession.getInstance().getLoggedUser();
 	}
 }
--- complete/src/test/java/refactoring/kata/trip/TripServiceTest.java
+++ complete/src/test/java/refactoring/kata/trip/TripServiceTest.java
@@ -1,15 +1,26 @@
 package refactoring.kata.trip;
 
 import org.junit.Test;
-
-import static org.junit.Assert.fail;
+import refactoring.kata.exception.UserNotLoggedInException;
+import refactoring.kata.user.User;
 
 public class TripServiceTest {
 
-    // TODO
+    private static final User GUEST = null;
+    private static final User UNUSED_USER = null;
 
-    @Test
-    public void implement_tests() {
-        fail("tests not implemented");
+    private User loggedInUser;
+
+    @Test(expected = UserNotLoggedInException.class) public void
+    should_throw_an_exception_when_a_not_logged_in_user_tries_to_view_the_trips_of_any_other_user() throws UserNotLoggedInException {
+        loggedInUser = GUEST;
+        new TestableTripService().getTripsByUser(UNUSED_USER);
+    }
+
+    private class TestableTripService extends TripService {
+        @Override
+        protected User getLoggedInUser() {
+            return loggedInUser;
+        }
     }
 }
